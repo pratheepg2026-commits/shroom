ee16c977039a9c5b1800265d36fbf6789f71f68e
pratheepg2026-commits <pratheepg2026@gmail.com>
2025-11-17 02:52:10 +0530

    Update InvoiceGenerator.tsx

--- frontend/components/InvoiceGenerator.tsx
+++ frontend/components/InvoiceGenerator.tsx
@@ -257,7 +257,7 @@ const InvoiceGenerator: React.FC = () => {
                     <img 
                         src={Logo} 
                         alt="Logo"
-                        className="h-20 w-auto object-contain scale-150"
+                        className="h-20 w-auto object-contain scale-100"
                     />
                 </div>
 
